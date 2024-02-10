1584c7659ad86e2b20f6f4c8f642ff3ffc2189a9
Nicolas <nicolasgfalabella@gmail.com>
2024-02-09 22:11:38 -0300

    Modificaciones form

--- src/app/component/Loguin/Loguin.jsx
+++ src/app/component/Loguin/Loguin.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 
 const Loguin = () => {
   return (
-    <section className='text-white bg-black/75 w-[95%] mx-auto h-[300px] flex flex-col gap-y-3 items-center justify-center rounded-md '>
-        <h3 className='text-center font-bold'> ¡Hola! Ingresá tu e-mail</h3>
-        <input placeholder='Correo electrónico' className='rounded-md w-[80%] mx-auto py-2 placeholder:pl-2 placeholder:text-gray-300 bg-gray-700 placeholder:text-sm'/> 
-        <button className='bg-lemon text-gray-900 font-bold rounded-md w-[80%] mx-auto py-2'> Continuar </button>
+    <section className='text-white bg-black/75 w-[95%] mx-auto h-[250px] flex flex-col gap-y-3 items-center justify-center rounded-md '>
+        <h3 className='text-center font-bold'> ¡Hola! Ingresá tu e-mail </h3>
+        
+        <input 
+        type="email"
+        placeholder='Correo electrónico' 
+        className='border-[1px] border-transparent focus:border-lemon focus:outline-none rounded-md w-[80%] mx-auto py-2 placeholder:text-gray-300 bg-gray-700 placeholder:text-sm pl-4 font-normal'/> 
+        
+        <button className='bg-lemon border-2 border-transparent text-gray-900 font-bold rounded-md w-[80%] mx-auto py-2
+                            hover:bg-black/70 hover:text-lemon hover:border-lemon transition-colors duration-300'> Continuar </button>
     </section>
   )
 }
