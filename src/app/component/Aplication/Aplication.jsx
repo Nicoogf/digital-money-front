b45c58b3f171cce1fadbd1235e0dbb56704dd9a4
Nicolas <nicolasgfalabella@gmail.com>
2024-02-09 22:36:24 -0300

    Form password

--- src/app/component/Aplication/Aplication.jsx
+++ src/app/component/Aplication/Aplication.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Nav from '../Nav/Nav'
-import Loguin from '../Loguin/Loguin'
+import LoguinComponent from '../Loguin/Loguin'
 import Footer from '../Footer/Footer'
+import PasswordComponent from '../Password/PasswordComponent'
 
 const Aplication = () => {
   return (
     <div className="absolute top-0 w-full h-screen flex flex-col items-center justify-center">
          <Nav />
-         <Loguin />
+         { /* <LoguinComponent /> */ } 
+          <PasswordComponent />
          <Footer />
     </div>
   )
--- src/app/component/Password/PasswordComponent.jsx
+++ src/app/component/Password/PasswordComponent.jsx
@@ -1,8 +1,7 @@
 import Link from 'next/link'
 import React from 'react'
 
-
-const LoguinComponent = () => {
+const PasswordComponent = () => {
   return (
     <section className='text-white bg-black/75 w-[95%] mx-auto h-[220px]  flex flex-col gap-y-3 items-center justify-center rounded-md max-w-[420px]
     sm:h-[250px]
@@ -10,12 +9,11 @@ const LoguinComponent = () => {
     lg:w-[55%]'>
         <h3 className='text-center font-bold text-lg mb-2
         md:text-xl
-        lg:text-2xl'> ¡Hola! Ingresá tu e-mail </h3>
+        lg:text-2xl'> Ingresá tu contraseña </h3>
         
-        <input 
-        autofocus
-        type="email"
-        placeholder='Correo electrónico' 
+        <input         
+        type="password"
+        placeholder='Contraseña' 
         className='border-[1px] border-gray-700 focus:border-lemon focus:outline-none rounded-md w-[80%] mx-auto py-3 placeholder:text-gray-400 bg-black/80 placeholder:text-sm pl-4 font-normal'/> 
 
         <button className='bg-lemon border-2 border-transparent text-gray-900 font-bold rounded-md w-[80%] mx-auto py-2 hover:bg-black/70 hover:text-lemon hover:border-lemon transition-colors duration-300'> 
@@ -29,4 +27,4 @@ const LoguinComponent = () => {
   )
 }
 
-export default LoguinComponent
+export default PasswordComponent
