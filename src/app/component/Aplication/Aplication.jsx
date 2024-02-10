5ef94b6710dfc5d3ceaec24680624b6e62ee0aa0
Nicolas <nicolasgfalabella@gmail.com>
2024-02-09 22:40:39 -0300

    Confirm Code

--- src/app/component/Aplication/Aplication.jsx
+++ src/app/component/Aplication/Aplication.jsx
@@ -3,13 +3,15 @@ import Nav from '../Nav/Nav'
 import LoguinComponent from '../Loguin/Loguin'
 import Footer from '../Footer/Footer'
 import PasswordComponent from '../Password/PasswordComponent'
+import CodeValidator from '../CodeValidator/CodeValidator'
 
 const Aplication = () => {
   return (
     <div className="absolute top-0 w-full h-screen flex flex-col items-center justify-center">
          <Nav />
          { /* <LoguinComponent /> */ } 
-          <PasswordComponent />
+         { /*  <PasswordComponent /> */ } 
+         <CodeValidator />
          <Footer />
     </div>
   )
---
+++ src/app/component/CodeValidator/CodeValidator.jsx
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+import React from 'react'
+
+const CodeValidator = () => {
+  return (
+    <section className='text-white bg-black/75 w-[95%] mx-auto h-[220px]  flex flex-col gap-y-3 items-center justify-center rounded-md max-w-[420px]
+    sm:h-[250px]
+    md:w-[90%] md:h-[300px]
+    lg:w-[55%]'>
+        <h3 className='text-center font-bold text-lg mb-2
+        md:text-xl
+        lg:text-2xl'>Ingresa el codigo de verificacion </h3>
+        
+        <input         
+        type="text"
+        placeholder='Código' 
+        className='border-[1px] border-gray-700 focus:border-lemon focus:outline-none rounded-md w-[80%] mx-auto py-3 placeholder:text-gray-400 bg-black/80 placeholder:text-sm pl-4 font-normal'/> 
+
+        <button className='bg-lemon border-2 border-transparent text-gray-900 font-bold rounded-md w-[80%] mx-auto py-2 hover:bg-black/70 hover:text-lemon hover:border-lemon transition-colors duration-300'> 
+               Ingresar
+        </button>
+
+        <Link href={""}>
+          <p className='text-xs underline text-lemon mt-2 cursor-pointer'>  ¿ No te ha llegado el codigo ? </p>
+        </Link>
+    </section>
+  )
+}
+
+export default CodeValidator
